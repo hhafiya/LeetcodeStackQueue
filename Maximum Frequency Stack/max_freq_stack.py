5f9d9263dc882dd492ed58b8a696dfe08e21d5e6
Your Name <you@example.com>
2024-04-15 16:50:13 +0300

    fixed last task to pass all tests and changed png to proper ones with all tests

--- Maximum Frequency Stack/max_freq_stack.py
+++ Maximum Frequency Stack/max_freq_stack.py
@@ -3,7 +3,7 @@ from collections import deque
 class FreqElement:
     def __init__(self, val):
         self.val = val
-        self.count = 0
+        self.count = 1
     
     def __repr__(self):
         return f'{self.val}'
@@ -13,34 +13,23 @@ class FreqStack:
         self.stack = deque()
 
     def push(self, val: int) -> None:
-        exist = None
         val_obj = FreqElement(val)
-        for i in self.stack:
-            if i.val == val_obj.val:
-                exist = i
+        for elem in self.stack:
+            if elem.val == val:
+                new_count = elem.count + 1
+                val_obj.count += new_count
                 break
-        if exist:
-            exist.count += 1
-            val_obj.count = exist.count
-            self.stack.append(val_obj)
-        else:
-            val_obj.count = 1
-            self.stack.append(val_obj)
+        self.stack.appendleft(val_obj)
 
     def pop(self) -> int:
-        if not self.stack:
-            return None
-        most_freq = None
         current_freq = 0
-        for i in self.stack:
-            if i.count >= current_freq:
-                current_freq = i.count
-                most_freq = i
-        print(most_freq, current_freq)
-        if most_freq:
-            self.stack.remove(most_freq)
-            for i in self.stack:
-                if i.val == most_freq.val:
-                    i.count -= 1
-            return most_freq.val
-        return None
+        for elem in self.stack:
+            if elem.count > current_freq:
+                current_freq = elem.count
+                most_freq = elem
+        self.stack.remove(most_freq)
+        return most_freq.val
+
+
+
+
